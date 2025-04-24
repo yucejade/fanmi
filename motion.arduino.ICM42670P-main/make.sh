ec6fa3b9d2ca8b9581dfb2fe20448824d541f2da
yucejade <121438331@qq.com>
2025-04-24 18:40:05 +0800

    修改gpiod依赖

--- motion.arduino.ICM42670P-main/make.sh
+++ motion.arduino.ICM42670P-main/make.sh
@@ -1,13 +1,26 @@
+#! /usr/bin/bash -f
+
 clear
 reset
 # 
 export FmDev=$(pwd)
+export FmInstallPrefix="${FmDev}/build/package"
 # 
 
-rm -rf build
-cmake -B build -S . -DCMAKE_BUILD_TYPE=debug ..
-cd build
-make install
-# cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/home/kevin/Dev/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -Wnarrowing
-# cmake --build build
-# 
+rm build -fr
+
+cd ${FmDev}/thirdparty/libgpiod-1.6.3
+${FmDev}/thirdparty/libgpiod-1.6.3/autogen.sh --enable-tools=yes
+${FmDev}/thirdparty/libgpiod-1.6.3/configure  --prefix=${FmInstallPrefix}
+make -f ${FmDev}/thirdparty/libgpiod-1.6.3/Makefile
+# sudo make -f ${FmDev}/thirdparty/libgpiod/Makefile install
+cd -
+
+#cmake -B build -S . -DCMAKE_BUILD_TYPE=debug ..
+#cd build
+#make install
+
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/home/kevin/Dev/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -Wnarrowing
+cmake --build build
+cmake --build install
+ 
